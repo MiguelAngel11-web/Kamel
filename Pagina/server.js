3c7c9be8e1ffce9ac265fc8c2d61060e31f961cc
MiguelAngel11-web <miguel_asc11@hotmail.com>
2020-06-28 17:07:01 -0500

    Cambie el dirname del serverjs

--- Pagina/server.js
+++ Pagina/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/myapp"));
+app.use(express.static(__dirname + "/dist/pagina"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/myapp/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/pagina/index.html"));
 });
 app.listen(process.env.PORT || 8080);
