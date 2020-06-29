849873e69f221fe0916c612150e75e44ae88bde9
MiguelAngel11-web <miguel_asc11@hotmail.com>
2020-06-29 16:34:14 -0500

    Subiendo a Heroku con firebase

--- Pagina/server.js
+++ Pagina/server.js
@@ -3,6 +3,17 @@ const path = require("path");
 
 const app = express();
 
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://kamel-6e19d.firebaseio.com"
+});
+
+
 app.use(express.static(__dirname + "/dist/pagina"));
 
 
